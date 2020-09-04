f1b2ad6704347cfc4c82c94e93a7a115b451f9aa
dragonwang-hub <915614879@qq.com>
2020-09-04 17:35:21 +0800

    [dragon] feat: src/exercise_02/characterCounter.js

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,16 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (string === undefined || string === null) return 0;
+  let result = string.length;
+  if (prediction === undefined) return result;
+
+  let count = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (prediction(string[i])) {
+      count += 1;
+    }
+  }
+  result = count;
+  return result;
 }
