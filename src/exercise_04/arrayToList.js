b0ac798145bf9ce054af1add80c86552bfc7d61a
dragonwang-hub <915614879@qq.com>
2020-09-05 11:54:09 +0800

    [dragon] feat: Finish  src/exercise_04/arrayToList.js

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,23 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  function ListNode(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+  const firstNode = new ListNode(array[0], null);
+  let currNode = firstNode;
+  for (let i = 1; i < array.length; i += 1) {
+    const nextNode = new ListNode(null, null);
+    currNode.next = nextNode;
+    nextNode.value = array[i];
+    currNode = currNode.next;
+  }
+  return firstNode;
 }
