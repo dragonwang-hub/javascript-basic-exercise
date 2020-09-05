527f6b831a8ddbd30c946eb5c1ffbe4869aa5e85
dragonwang-hub <915614879@qq.com>
2020-09-05 13:33:04 +0800

    [dragon] feat: Finish  src/exercise_05/flattenArray.js

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,20 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (array === undefined || array === null) {
+    throw new Error('Flatten undefined or null array');
+  }
 
-  throw new Error('Please delete this line and implement the function');
+  const result = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] instanceof Array) {
+      const subArray = array[i];
+      for (let j = 0; j < subArray.length; j += 1) {
+        result.push(subArray[j]);
+      }
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
 }
